1804f379c70df8c65d9bf8d374f742c659af0fa0
Ronald <ronald.chow@student.unsw.edu.au>
2024-03-11 02:25:26 +1100

    Wrote pseudocode tests

---
+++ src/test/sprint1.test.js
@@ -0,0 +1,16 @@
+describe("Sprint 1 system tests", () => {
+    // Pseudocode
+    // Register (invalid)
+    // Register User 1 (valid)
+    // Register User 2 (valid)
+    // Login User 1 with User 2's password (invalid)
+    // Login User 2 with User 1's password (invalid)
+    // Login User 1 (valid)
+    // Login User 2 (valid)
+    // User 1 Upload Invoice (valid)
+    // User 1 Retrieve Invoice (invalid)
+    // User 1 Retrieve Invoice (valid)
+    // User 2 Upload Invoice (valid)
+    // User 2 Retrieve Invoice (invalid)
+    // User 2 Retrieve Invoice (valid)
+});
