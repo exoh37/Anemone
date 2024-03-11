a1a1caea2a45d3c9d2e7286f670fff8e75ccd73f
Ronald <ronald.chow@student.unsw.edu.au>
2024-03-11 11:47:52 +1100

    Added clear before everything

--- src/test/sprint1.test.js
+++ src/test/sprint1.test.js
@@ -12,26 +12,18 @@ const invalidToken = "thisIsAnInvalidToken";
 const request = require("supertest");
 const app = require("../main/server"); 
 
-describe("Sprint 1 system tests", () => {
-    // System Test Flowchart
-    // Register (invalid)
-    // Register User 1 (valid)
-    // Register User 2 (valid)
-    // Login User 1 with User 2's password (invalid)
-    // Login User 2 with User 1's password (invalid)
-    // Login User 1 (valid)
-    // Login User 2 (valid)
-    // User 1 Upload Invoice (valid)
-    // User 2 Upload Invoice (valid)
-    // User 1 Retrieve Invoice (invalid)
-    // User 2 Retrieve Invoice (invalid)
-    // User 1 Retrieve Invoice (valid)
-    // User 2 Retrieve Invoice (valid)
-    // Fake token Retrieve Invoice (invalid)
-
-    it("Invalid registration", async () => {
-        // Register fail
+describe("Sprint 1 system test(s)", () => {
+    it("System Test", async () => {
+        // Clear
         let response = await request(app)
+            .delete("/clear");
+        expect(response.statusCode).toBe(200);
+        expect(response).toStrictEqual({
+            success: true
+        });
+
+        // Register fail
+        response = await request(app)
             .post("/users")
             .send({ username: invalidUsername, email: validEmail1, password: validPassword1 });
         expect(response.statusCode).toBe(400);
