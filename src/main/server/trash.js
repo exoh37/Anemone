07fdb4894e4bea89356c39e9af2c4d8afa74d430
Ronald <ronald.chow@student.unsw.edu.au>
2024-03-22 17:12:59 +1100

    Completed implementation of list trash items

--- src/main/server/trash.js
+++ src/main/server/trash.js
@@ -1,1 +1,35 @@
-// const other = require("./other.js");
+const other = require("./other.js");
+
+function listTrashItems(token) {
+    const tokenValidation = auth.tokenIsValid(token);
+    if (!tokenValidation.valid) {
+        return {
+            code: 401,
+            ret: {
+                success: false,
+                error: "Token is empty or invalid"
+            }
+        };
+    }
+
+    const jsonData = other.getInvoiceData();
+
+    // This is assuming that the invoices are appended to the end of the JSON array as
+    // they get deleted so that the invoices array can be returned in the correct order
+    let invoices = jsonData.filter(invoice => invoice.owner === tokenValidation.username);
+    invoices = invoices.map(obj => {
+        // Remove owner from the array
+        const { owner, ...rest } = obj;
+        return rest;
+    })
+
+    return {
+        code: 200,
+        ret: {
+            success: true,
+            invoices: invoices
+        }
+    }
+}
+
+module.exports = { listTrashItems };
