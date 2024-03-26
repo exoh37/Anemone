e52b11d0cb059ee8338f4cff7c6591cd0eee1ff4
godaiflying <31681293+godaiflying@users.noreply.github.com>
2024-03-26 22:32:53 +1100

    Delete Trash implenetation finished
    Just need to write server route and tests

--- src/main/server/trash.js
+++ src/main/server/trash.js
@@ -1,5 +1,56 @@
-//const auth = require("./auth.js");
-//const other = require("./other.js");
+const { json } = require("express");
+const auth = require("./auth.js");
+const other = require("./other.js");
 
 
 
+function deleteTrash(invoiceId,token) { 
+    const tokenValidation = auth.tokenIsValid(token);
+    if (!tokenValidation.valid) {
+        return {
+            code: 401,
+            ret: {
+                success: false,
+                error: "Token is empty or invalid"
+            }
+        };
+    }
+    
+    const jsonData = other.getInvoiceData();
+    const invoice = jsonData.find(invoice => invoice.invoiceId === parseInt(invoiceId));
+    const invoiceIndex = jsonData.findIndex(invoice => invoice.invoiceId === parseInt(invoiceId));
+    if (invoice === undefined) {
+        return {
+            code: 400,
+            ret: {
+                success: false,
+                error: `invoiceId '${invoiceId}' does not refer to an existing invoice`
+            }
+        };
+    } else if (invoice.trashed !== true) {
+        return {
+            code: 400,
+            ret: {
+                success: false,
+                error: `invoiceId refers to an invoice not in the trash '${invoiceId}'`
+            }
+        };
+    } 
+
+    else if (invoice.owner !== tokenValidation.username) {
+        return {
+            code: 403,
+            ret: {
+                success: false,
+                error: "Valid token and invoiceId are provided, but user is not owner of this invoice"
+            }
+        };
+    }
+
+    jsonData.splice(invoiceIndex, 1);
+    
+    other.setInvoiceData(jsonData);
+    return {success: true};
+}
+
+module.exports = { deleteTrash };
