c156ffc287dd4a92d7f32657887c8dc451e8687c
Ronald <ronald.chow@student.unsw.edu.au>
2024-03-30 15:32:00 +1100

    Completed Sprint 3 System Tests

--- src/test/sprint3.test.js
+++ src/test/sprint3.test.js
@@ -8,7 +8,16 @@ const mockInvoice1 = { file: { amount: 123.45 } };
 const mockInvoice2 = { file: { amount: 42.42 } };
 const mockInvoice3 = { file: { amount: 999.99 } };
 const mockInvoice4 = { file: { amount: 67.89 } };
-const invalidToken = "thisIsAnInvalidToken";
+const modifyInvoice1 = {
+    newInvoiceName: "this is new",
+    newAmount: 37,
+    newDate: ""
+}
+const modifyInvoice3 = {
+    newInvoiceName: "but this is newer",
+    newAmount: 1.11,
+    newDate: ""
+}
 
 const request = require("supertest");
 const assert = require("assert");
@@ -59,6 +68,22 @@ describe("Sprint 3 system test(s)", function() {
             .post("/users/login")
             .send({ username: validUsername2, password: validPassword2 });
 
+        // Empty list test
+        const emptyList1 = await request(app)
+            .get("/invoices")
+            .set("token", user1.body.token)
+            .expect(200)
+            .expect("Content-Type", /application\/json/);
+
+        const emptyList2 = await request(app)
+            .get("/invoices")
+            .set("token", user2.body.token)
+            .expect(200)
+            .expect("Content-Type", /application\/json/);
+        
+        assert.strictEqual(emptyList1.body.invoices.length, 0);
+        assert.strictEqual(emptyList2.body.invoices.length, 0);
+
         // Upload invoices: invoice1 + invoice2 belong to user1, invoice3 + invoice4 belong to user2
         const invoice1 = await request(app)
             .post("/invoices")
@@ -104,7 +129,7 @@ describe("Sprint 3 system test(s)", function() {
             .expect(200)
             .expect("Content-Type", /application\/json/);
 
-        // Deleting 1 invoice from each user
+        // Move to trash (1 + 3)
         await request(app)
             .delete(`/invoices/${invoice1.body.invoiceId}`)
             .set("token", user1.body.token)
@@ -124,7 +149,7 @@ describe("Sprint 3 system test(s)", function() {
         assertListIndexHasInvoice("/trash", 0, returnedInvoice3, user2, true);
         assertListIndexHasInvoice("/invoices", 0, returnedInvoice4, user2, false);
 
-        // Deleting other invoice from each user
+        // Move to trash (2 + 4)
         await request(app)
             .delete(`/invoices/${invoice2.body.invoiceId}`)
             .set("token", user1.body.token)
@@ -144,6 +169,94 @@ describe("Sprint 3 system test(s)", function() {
         assertListIndexHasInvoice("/trash", 0, returnedInvoice3, user2, true);
         assertListIndexHasInvoice("/trash", 1, returnedInvoice4, user2, true);
 
-        // Restore
+        // Restore (1 + 3)
+        await request(app)
+            .post(`/trash/${invoice1.body.invoiceId}/restore`)
+            .set("token", user1.body.token)
+            .expect(200)
+            .expect("Content-Type", /application\/json/)
+            .expect({"success": true});
+
+        await request(app)
+            .post(`/trash/${invoice3.body.invoiceId}/restore`)
+            .set("token", user2.body.token)
+            .expect(200)
+            .expect("Content-Type", /application\/json/)
+            .expect({"success": true});
+
+        assertListIndexHasInvoice("/invoices", 0, returnedInvoice1, user1, false);
+        assertListIndexHasInvoice("/trash", 0, returnedInvoice2, user1, true);
+        assertListIndexHasInvoice("/invoices", 0, returnedInvoice3, user2, false);
+        assertListIndexHasInvoice("/trash", 0, returnedInvoice4, user2, true);
+
+        // Modify (1 + 3)
+        const modifiedInvoice1 = await request(app)
+            .put(`/invoices/${invoice1.body.invoiceId}`)
+            .set("token", user1.body.token)
+            .send(modifyInvoice1)
+            .expect(200)
+            .expect("Content-Type", /application\/json/)
+        
+        // Unchanging invoiceId, changed name, changed amount, unchanging date
+        assert.strictEqual(modifiedInvoice1.body.success, true);
+        assert.strictEqual(modifiedInvoice1.body.invoice.invoiceId, invoice1.body.invoiceId);
+        assert.strictEqual(modifiedInvoice1.body.invoice.invoiceName, modifyInvoice1.newInvoiceName);
+        assert.strictEqual(modifiedInvoice1.body.invoice.amount, modifyInvoice1.newAmount);
+        assert.strictEqual(modifiedInvoice1.body.invoice.date, returnedInvoice1.body.date);
+        assert.strictEqual(modifiedInvoice1.body.invoice.trashed, false);
+
+        const modifiedInvoice3 = await request(app)
+            .put(`/invoices/${invoice3.body.invoiceId}`)
+            .set("token", user2.body.token)
+            .send(modifyInvoice3)
+            .expect(200)
+            .expect("Content-Type", /application\/json/)
+
+        // Unchanging invoiceId, changed name, changed amount, unchanging date
+        assert.strictEqual(modifiedInvoice3.body.success, true);
+        assert.strictEqual(modifiedInvoice3.body.invoice.invoiceId, invoice3.body.invoiceId);
+        assert.strictEqual(modifiedInvoice3.body.invoice.invoiceName, modifyInvoice3.newInvoiceName);
+        assert.strictEqual(modifiedInvoice3.body.invoice.amount, modifyInvoice3.newAmount);
+        assert.strictEqual(modifiedInvoice3.body.invoice.date, returnedInvoice3.body.date);
+        assert.strictEqual(modifiedInvoice3.body.invoice.trashed, false);
+
+        // Assert the modified invoices are still in the system
+        assertListIndexHasInvoice("/invoices", 0, modifiedInvoice1, user1, false);
+        assertListIndexHasInvoice("/trash", 0, returnedInvoice2, user1, true);
+        assertListIndexHasInvoice("/invoices", 0, modifiedInvoice3, user2, false);
+        assertListIndexHasInvoice("/trash", 0, returnedInvoice4, user2, true);
+
+        // Delete (2 + 4)
+        await request(app)
+            .delete(`/trash/${invoice2.body.invoiceId}`)
+            .set("token", user1.body.token)
+            .expect(200)
+            .expect("Content-Type", /application\/json/)
+            .expect({"success": true});
+
+        await request(app)
+            .delete(`/trash/${invoice4.body.invoiceId}`)
+            .set("token", user2.body.token)
+            .expect(200)
+            .expect("Content-Type", /application\/json/)
+            .expect({"success": true});
+
+        assertListIndexHasInvoice("/invoices", 0, modifiedInvoice1, user1, false);
+        assertListIndexHasInvoice("/invoices", 0, modifiedInvoice3, user2, false);
+        
+        const trashList1 = await request(app)
+            .get("/trash")
+            .set("token", user1.body.token)
+            .expect(200)
+            .expect("Content-Type", /application\/json/);
+
+        const trashList2 = await request(app)
+            .get("/trash")
+            .set("token", user2.body.token)
+            .expect(200)
+            .expect("Content-Type", /application\/json/);
+        
+        assert.strictEqual(trashList1.body.invoices.length, 0);
+        assert.strictEqual(trashList2.body.invoices.length, 0);
     });
 });
