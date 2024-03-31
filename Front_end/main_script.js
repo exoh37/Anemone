32b796ede43ef63584ce7f1829280d2d4b0b27b3
godaiflying <31681293+godaiflying@users.noreply.github.com>
2024-03-31 18:54:29 +1100

    fixing upload

--- Front_end/main_script.js
+++ Front_end/main_script.js
@@ -24,3 +24,17 @@ function addItem() {
   }
   return false;
 }
+
+
+function readURL(input) {
+  if (input.files && input.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+          $('#blah')
+              .attr('src', e.target.result);
+      };
+
+      reader.readAsDataURL(input.files[0]);
+  }
+}
