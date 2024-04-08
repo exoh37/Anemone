00067a8f826f3b598c55d88c6e674215cdcf232e
anemonade <annie.thai24@gmail.com>
2024-04-08 14:35:30 +1000

    Updated tests for clear to test based on SQL data persistence

--- src/test/clear.test.js
+++ src/test/clear.test.js
@@ -4,18 +4,16 @@ const server = require("../main/server");
 
 // Sample test using supertest and mocha
 describe("Testing route DELETE /clear", function() {
-    it("Valid return status and return body", function(done) {
-        request(app)
+    it("Valid return status and return body", async function() {
+        await request(app)
             // Route
-            .delete("/clear")
+            .delete("/clear2")
             // Status Code
             .expect(200)
             // Regex for checking content type to ignore charset
             .expect("Content-Type", /application\/json/)
             // Expected JSON body
             .expect({"success": true})
-            // Call done
-            .end(done);
     });
 });
 
