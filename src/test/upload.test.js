33562704949c1c9f3199cf00f65a017eb5b0e181
anemonade <annie.thai24@gmail.com>
2024-04-08 22:44:49 +1000

    Updated upload tests to use new postgres routes

--- src/test/upload.test.js
+++ src/test/upload.test.js
@@ -1,19 +1,29 @@
 
-const validUsername1 = "validUsername1",
-    validEmail1 = "test123@gmail.com",
-    validPassword1 = "ThisIsSecure!123",
+const validUsername1 = "validUsername1";
+const validEmail1 = "test123@gmail.com";
+const validPassword1 = "ThisIsSecure!123";
 
-    mockInvoice1 = { file: { amount: 125.45 } };
+const mockInvoice1 = `
+    <invoices>
+        <invoice>
+            <invoiceName>Invoice 1</invoiceName>
+            <amount>123.45</amount>
+            <date>2024-04-08</date>
+            <trashed>false</trashed>
+            <owner>validUsername1</owner>
+        </invoice>
+    </invoices>
+`;
 
 const request = require("supertest");
 const assert = require("assert");
 const app = require("../main/server");
-const server = require("../main/server"),
+const server = require("../main/server");
 
-    invalid_token = 0;
+const invalid_token = 0;
 
 describe("Testing route POST /invoices", function() {
-    it("tests for Retrieving Invoices", async function() {
+    it("tests for Uploading Invoices", async function() {
         // Setup user and login process
         await request(app)
             .delete("/clear2")
