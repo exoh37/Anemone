511a2df21222f53849cf0c379f114bdb1cf8c682
anemonade <annie.thai24@gmail.com>
2024-04-09 01:14:03 +1000

    Changed password type

--- client.js
+++ client.js
@@ -4,7 +4,7 @@ async function createTables() {
     const client = await pool.connect();
     try {
         await client.query("CREATE TABLE tokens(tokenid VARCHAR(36) PRIMARY KEY, username Text NOT NULL, expiration BIGINT NOT NULL)");
-        await client.query("CREATE TABLE users(username Text NOT NULL, email Text NOT NULL, password BIGINT NOT NULL)");
+        await client.query("CREATE TABLE users(username Text NOT NULL, email Text NOT NULL, password Text NOT NULL)");
         await client.query("CREATE TABLE invoices(invoiceid bigint PRIMARY KEY, invoice XML)");
     
     } catch (error) {
