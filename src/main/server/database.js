b4867b07ff675275130b5829c93a7066e90743e5
anemonade <annie.thai24@gmail.com>
2024-04-08 22:32:20 +1000

    Updates database to connect to postgres

--- src/main/server/database.js
+++ src/main/server/database.js
@@ -1,12 +1,12 @@
-const mysql = require('mysql2/promise');
-const dotenv = require('dotenv');
-dotenv.config()
+const {Pool} = require("pg");
+const dotenv = require("dotenv");
+dotenv.config();
 
-const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE
-})
+const pool = new Pool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+});
 
 module.exports = pool;
