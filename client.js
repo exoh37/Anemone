2f8e3ac47c439a60a6bf4057042a5a0c494ce680
anemonade <annie.thai24@gmail.com>
2024-04-11 23:41:02 +1000

    Added additional tables for the database

--- client.js
+++ client.js
@@ -6,6 +6,9 @@ async function createTables() {
         await client.query("CREATE TABLE tokens(tokenid VARCHAR(36) PRIMARY KEY, username Text NOT NULL, expiration BIGINT NOT NULL)");
         await client.query("CREATE TABLE users(username Text NOT NULL, email Text NOT NULL, password Text NOT NULL)");
         await client.query("CREATE TABLE invoices(invoiceid bigint PRIMARY KEY, invoice XML)");
+        await client.query("CREATE TABLE invoiceInfo(invoiceid bigint PRIMARY KEY, invoicename Text NOT NULL, owner Text NOT NULL, amount Float NOT NULL, date Text NOT NULL)");
+        await client.query("CREATE TABLE trash(invoiceid bigint PRIMARY KEY, invoice XML)");
+        await client.query("CREATE TABLE trashInfo(invoiceid bigint PRIMARY KEY, invoicename Text NOT NULL, owner Text NOT NULL, amount Float NOT NULL, date Text NOT NULL)");
     
     } catch (error) {
         console.log(error)
