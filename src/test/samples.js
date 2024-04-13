7bfc0e49891cdb4aae4a02e83d890ce1989b0ce1
Ronald <ronald.chow@student.unsw.edu.au>
2024-04-14 01:28:56 +1000

    Removed deprecated test files

--- src/test/samples.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-const sampleXML = "./sample/books.xml";
-
-function getSampleInvoice() {
-    return fs.readFileSync(require.resolve(sampleXML), "utf8");
-}
-
-module.exports = { getSampleInvoice };
